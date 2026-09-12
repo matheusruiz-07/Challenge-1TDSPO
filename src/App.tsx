5f097318f50a8d79431f3f0297acadaf62c62e72
matheusruiz-07 <matheus.rgiatti@gmail.com>
2026-09-11 22:22:34 -0300

    Adiciona a pagina da solucao com missoes e progresso

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Contato from "./pages/Contato";
 import Home from "./pages/Home";
 import Integrantes from "./pages/Integrantes";
 import Sobre from "./pages/Sobre";
+import Solucao from "./pages/Solucao";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="sobre" element={<Sobre />} />
+          <Route path="solucao" element={<Solucao />} />
           <Route path="integrantes" element={<Integrantes />} />
           <Route path="contato" element={<Contato />} />
           <Route
