fa6e8ff3f103db558a83a9c5195514731e470a89
matheusruiz-07 <matheus.rgiatti@gmail.com>
2026-09-11 22:21:54 -0300

    Adiciona a pagina de integrantes e registra a rota de Sobre

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import Contato from "./pages/Contato";
 import Home from "./pages/Home";
+import Integrantes from "./pages/Integrantes";
+import Sobre from "./pages/Sobre";
 
 export default function App() {
   return (
@@ -9,6 +11,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="sobre" element={<Sobre />} />
+          <Route path="integrantes" element={<Integrantes />} />
           <Route path="contato" element={<Contato />} />
           <Route
             path="*"
